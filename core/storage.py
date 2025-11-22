2cf1162a461dd6649af136260091c166f7bd575d
4B-Cycle <b.eloin@students.ephec.be>
2025-11-22 21:09:09 +0100

    base du stockage

--- core/storage.py
+++ core/storage.py
@@ -1,3 +1,37 @@
 """
 class StorageManager
 """
+
+import sqlite3
+
+#Création/Ouverture db
+db = sqlite3.connect('storage.db')
+
+#Pour créer les requetes
+cursor = db.cursor()
+
+#Création table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS categorie(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL
+)
+''')
+
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS carte(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    question TEXT NOT NULL,
+    reponse TEXT NOT NULL,
+    categorie_id INTEGER,
+    FOREIGN KEY(categorie_id) REFERENCES categorie(id)
+
+)
+''')
+
+cursor.execute('''INSERT INTO categorie (nom) VALUES ('Science')''')
+
+db.commit()
+
+db.close()
